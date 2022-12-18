ca211b1b2f7e9620c92b60d3f9f035680e59c8ee
Haighstrom <simon.haigh@gmail.com>
2022-12-18 13:49:12 +0000

    Folder reorg

---
+++ Source/Tools/Ensure.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+
+namespace HaighFramework;
+
+public static class Ensure
+{
+    /// <summary>
+    /// Ensures that the specified argument is not less than zero.
+    /// </summary>
+    /// <param name="argument">The argument.</param>
+    /// <param name="argumentName">Name of the argument.</param>
+    /// <exception cref="ArgumentNullException"></exception>
+    [DebuggerStepThrough]
+    public static void ArgumentNotNegative(float argument, string argumentName)
+    {
+        if (argument < 0)
+        {
+            throw new ArgumentOutOfRangeException(argumentName);
+        }
+    }
+
+    /// <summary>
+    /// Ensures that the specified argument is not null.
+    /// </summary>
+    /// <param name="argument">The argument.</param>
+    /// <param name="argumentName">Name of the argument.</param>
+    /// <exception cref="ArgumentNullException"></exception>
+    [DebuggerStepThrough]
+    public static void ArgumentNotNull(object argument, string argumentName)
+    {
+        if (argument == null)
+        {
+            throw new ArgumentNullException(argumentName);
+        }
+    }
+}
