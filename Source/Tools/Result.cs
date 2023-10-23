f2606dc571cb309925796cbdf314f6244c4efb44
Haighstrom <simon.haigh@gmail.com>
2023-10-23 16:29:11 +0100

    Created Result utility. Closes #2

---
+++ Source/Tools/Result.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace HaighFramework;
+
+public readonly struct Result<TValue, TError>
+{
+    private Result(TValue value)
+    {
+        Value = value;
+        Error = default;
+        Success = true;
+    }
+
+    private Result(TError error)
+    {
+        Value = default;
+        Error = error;
+        Success = false;
+    }
+
+    public TValue? Value { get; }
+
+    public TError? Error { get; }
+
+    [MemberNotNullWhen(true, nameof(Value))]
+    public bool Success { get; }
+
+    [MemberNotNullWhen(false, nameof(Value))]
+    public readonly bool Failed => !Success;
+
+    public static implicit operator Result<TValue, TError>(TValue value) => new(value);
+
+    public static implicit operator Result<TValue, TError>(TError error) => new(error);
+}
